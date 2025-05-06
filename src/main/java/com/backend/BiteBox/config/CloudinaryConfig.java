a83580537b45e390530ba25e99f92ad84e1f728c
Ram Sharma <ram2213020@akgec.ac.in>
2025-05-06 20:35:18 +0530

    secured the secrete keys

--- src/main/java/com/backend/BiteBox/config/CloudinaryConfig.java
+++ src/main/java/com/backend/BiteBox/config/CloudinaryConfig.java
@@ -2,6 +2,7 @@ package com.backend.BiteBox.config;
 
 import com.cloudinary.Cloudinary;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,18 +12,27 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud.name}")
+    private String cloudName;
+    @Value("${cloudinary.api.key}")
+    private String API_KEY;
+    @Value("${cloudinary.api.secret}")
+    private String API_SECRET;
+
     @Bean
     public ModelMapper mapper() {
         return new ModelMapper();
     }
 
 
+
+
     @Bean
     public Cloudinary getCloudinary() {
         Map config = new HashMap<>();
-        config.put("cloud_name", "drwotnwuf");
-        config.put("api_key", "616372856761312");
-        config.put("api_secret", "_RA80hJ9fBxbBx4AimVXMM9xJb4");
+        config.put("cloud_name", cloudName);
+        config.put("api_key", API_KEY);
+        config.put("api_secret", API_SECRET);
         config.put("secure", true);
 
         return new Cloudinary(config);
